7da716b06852b1544f43157f5e3198574092618a
Caio Vieira <contato@caiovieira.com.br>
2021-08-02 21:02:47 -0300

    feat: add new component MediaMatch

---
+++ src/components/MediaMatch/stories.tsx
@@ -0,0 +1,27 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import MediaMatch from '.'
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch
+} as Meta
+
+export const Desktop: Story = (args) => (
+  <MediaMatch greaterThan="medium" {...args}>
+    {' '}
+    Only on Desktop
+  </MediaMatch>
+)
+
+export const Mobile: Story = (args) => (
+  <MediaMatch lessThan="medium" {...args}>
+    {' '}
+    Only on Mobile
+  </MediaMatch>
+)
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1'
+  }
+}
