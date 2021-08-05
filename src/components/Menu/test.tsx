76394d1c382af762a60e13bc80d404017a4a0a9b
Caio Vieira <contato@caiovieira.com.br>
2021-08-05 19:53:06 -0300

    test: add new component Menu

---
+++ src/components/Menu/test.tsx
@@ -0,0 +1,49 @@
+import { fireEvent, screen } from '@testing-library/react'
+import Menu from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+describe('<Menu />', () => {
+  test('Should render the menu', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  test('Should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // selecionar o nosso MenuFull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+    // verificar se o menu está escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+    //clicar no botão de abrir o menu e verificar se o menu está visível
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+    //clicar no botão de fechar o menu e verificar se o menu está escondido
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+  })
+
+  test('Should show register box when Logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+  })
+
+  test('Should show wishlist and account when logged in', () => {
+    renderWithTheme(<Menu username="Caio Vieira" />)
+
+    expect(screen.queryByText(/my account/i)).toBeInTheDocument()
+    expect(screen.queryByText(/wishlist/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+  })
+})
